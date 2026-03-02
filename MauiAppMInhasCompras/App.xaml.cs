12eca3d021e9ffa83a535472625633e761853969
saraiva791 <saraivaandre791@gmail.com>
2026-03-02 20:05:18 -0300

    voltei a trabalhar com windows

--- MauiAppMInhasCompras/App.xaml.cs
+++ MauiAppMInhasCompras/App.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace MauiAppMInhasCompras
+﻿using MauiAppMInhasCompras.Helpers;
+
+namespace MauiAppMInhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper DB
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras,bd3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+                
+        }
+
         public App()
         {
             InitializeComponent();
