2e99e5adecc5a26098842db31786f8d317faf51b
saraiva791 <saraivaandre791@gmail.com>
2026-03-18 19:17:09 -0300

    resolução de bug

--- MauiAppMInhasCompras/Models/Produto.cs
+++ MauiAppMInhasCompras/Models/Produto.cs
@@ -4,9 +4,22 @@ namespace MauiAppMInhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if (value == null) 
+                {
+                    throw new Exception("A descrição é obrigatória");
+                }
+
+                _descricao = value;
+            } 
+        }
         public double Quantidade { get; set; }
         public double Preco { get; set; }
 
