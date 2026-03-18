bb64018dc97418e754714694d7d0d858543349c8
saraiva791 <saraivaandre791@gmail.com>
2026-03-18 19:36:24 -0300

    ultima correção de bug

--- MauiAppMInhasCompras/Models/Produto.cs
+++ MauiAppMInhasCompras/Models/Produto.cs
@@ -5,6 +5,8 @@ namespace MauiAppMInhasCompras.Models
     public class Produto
     {
         string _descricao;
+        double _quantidade;
+        double _preco;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -20,8 +22,28 @@ namespace MauiAppMInhasCompras.Models
                 _descricao = value;
             } 
         }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public double Quantidade { 
+            get => _quantidade; 
+            set
+            {
+                if (value == 0) 
+                {
+                    throw new Exception("A quantidade é obrigatoria");
+                }
+                _quantidade = value;
+            } 
+        }
+        public double Preco { 
+            get => _preco;
+            set 
+            {
+                if (value == 0) 
+                {
+                    throw new Exception("O preço é obrigatorio");
+                }
+                _preco = value;
+            } 
+        }
 
         public double Total { get => Quantidade * Preco;}
         
